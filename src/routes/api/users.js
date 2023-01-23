c3ff14996dc8433bc18d4f3b97bfe1468386b51d
Nikolay Tankov <tankov.n.k@gmail.com>
2023-01-24 00:47:17 +0200

    hw-05 done

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -8,6 +8,7 @@ const {
 } = require("../../middlewares/valadationMiddleware");
 
 const { auth } = require("../../middlewares/auth");
+const { upload } = require("../../middlewares/upload");
 
 const { asyncWrapper } = require("../../helpers/apiHelpers");
 
@@ -16,15 +17,18 @@ const { login } = require("../../controllers/login");
 const { logout } = require("../../controllers/logout");
 const { updateSubsc } = require("../../controllers/updateSubsc");
 const { getCurrent } = require("../../controllers/currentUserController");
+const { updateAvatar } = require("../../controllers/updateAvatar");
 
 router.post("/signup", userRegisterValidation, asyncWrapper(signup));
-
 router.post("/login", userLoginValidation, asyncWrapper(login));
-
 router.get("/logout", auth, asyncWrapper(logout));
-
 router.patch("/", auth, patchUpdateSubscValidation, asyncWrapper(updateSubsc));
-
 router.get("/current", auth, asyncWrapper(getCurrent));
+router.patch(
+  "/avatars",
+  auth,
+  upload.single("avatar"),
+  asyncWrapper(updateAvatar)
+);
 
 module.exports = router;
